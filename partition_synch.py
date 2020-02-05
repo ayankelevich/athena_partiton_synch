23636154a9ca7799eaf252a64072a73bc0b7c7b7
Alex Yankelevich <alex@12chairs.com>
2020-02-05 16:26:53 -0500

    Added DROP functionality

--- partition_synch.py
+++ partition_synch.py
@@ -2,61 +2,73 @@ import boto3
 import sys
 import getopt
 
-
-# BUCKET = 'gsdp-segment-logs'
-# PREFIX = 'segment-logs/HpDYYfmrXr/'
-# DATABASE = 'gsdp_segment'
-# TABLE = 'hpdyyfmrxr_test'
-# PARTITION_NAME = 'partition_0'
-# PROFILE = 'vmware'
 DEFAULT_PROFILE = None
 DEFAULT_REGION = None
 DEFAULT_OUTPUT = 's3://aws-athena-query-results-513065973071-us-east-1/alter_table_log'
-USAGE_STR = 'Usage: partition_synch.py -t <database.table> [-p <profile>] [-r <region>] [-o <output>] [-v]'
+DEFAULT_DROP_MODE = 'yes'
+USAGE_STR = 'Usage: partition_synch.py -t <database.table> [-p <profile>] [-r <region>] [-o <output>] [-d <yes|no> [-v]'
 
 
 def main_session(argv):
-    full_table_name, profile, region, output, verbose = parse_parameters(argv)
+    full_table_name, profile, region, output, drop, verbose = parse_parameters(argv)
     database, table = full_table_name.split('.')
     session_handle = boto3.session.Session(profile_name=profile, region_name=region)
     bucket, prefix, partition_name_list = get_bucket_prefix_partition(session_handle, database, table)
     partition_value_set = get_table_partitions(session_handle, database, table)
     folder_set = get_bucket_directories(session_handle, bucket, prefix)
-    diff_set = folder_set.difference(partition_value_set)
+    missing_partition_set = folder_set.difference(partition_value_set)
+    extra_partition_set = partition_value_set.difference(folder_set)
     if verbose:
-        print('Table:', table)
-        print('Database:', database)
-        print('Profile:', profile)
-        print('Output:', output)
-        print('Region:', region)
-        print('Bucket', bucket)
-        print('Prefix:', prefix)
-        print('Partition Name:', partition_name_list)
+        print('\n',
+              "Table:", table, '\n',
+              "Database:", database, '\n',
+              "Profile:", profile, '\n',
+              "Output:", output, '\n',
+              "Region:", region, '\n',
+              "Bucket:", bucket, '\n',
+              "Prefix:", prefix, '\n'
+              "Drop Partitions:", drop)
+        print("Partition Name:", partition_name_list)
         print("Partition Values:", sorted(list(partition_value_set)))
         print("Folders:         ", sorted(list(folder_set)))
-        print('Partitions to add:', diff_set)
-    if len(diff_set) == 0:
+        print("Partitions to add:", missing_partition_set)
+        print("Partitions to drop:", extra_partition_set)
+    if len(missing_partition_set) == 0 and len(extra_partition_set) == 0:
         print("The {}.{} table partitions are up-to-date with s3://{}/{}".format(database, table, bucket, prefix))
         exit(0)
-    else:
+    if len(missing_partition_set) > 0:
         print("Adding missing partitions to the {}.{} table from s3://{}/{}".format(database, table, bucket, prefix))
-    # Can be changed to issue a single ALTER for all partitions
+        for ddl_str in adjust_partitions("ADD", missing_partition_set, partition_name_list, database, table, bucket, prefix):
+            if verbose:
+                print("DDL String:", ddl_str)
+            qid = execute_athena_command(session_handle, ddl_str, database, output)
+            if verbose:
+                print(qid)
+    if len(extra_partition_set) > 0 and drop == 'yes':
+        print("Dropping extra partitions to the {}.{} table from s3://{}/{}".format(database, table, bucket, prefix))
+        for ddl_str in adjust_partitions("DROP", extra_partition_set, partition_name_list, database, table, bucket, prefix):
+            if verbose:
+                print("DDL String:", ddl_str)
+            qid = execute_athena_command(session_handle, ddl_str, database, output)
+            if verbose:
+                print(qid)
 
-    for new_folder in diff_set:
+
+def adjust_partitions(p_operation, p_diff_set, p_partition_name_list, p_database, p_table, p_bucket, p_prefix):
+    # Can be changed to a single ADD / DROP for multiple partitions
+    for new_folder in p_diff_set:
         new_folder_list = new_folder.split('/')
         partition_str = ''
         for i in range(len(new_folder_list)):
             if partition_str != '':
                 partition_str += ', '
-            partition_str += partition_name_list[i] + " = " + "'" + new_folder_list[i] + "'"  # optimize
+            partition_str += p_partition_name_list[i] + " = " + "'" + new_folder_list[i] + "'"  # optimize
 
-        ddl_str = "ALTER TABLE {0}.{1} ADD PARTITION ({2}) " \
-                  " LOCATION 's3://{4}/{5}{3}'".format(database, table, partition_str, new_folder, bucket, prefix)
-        if verbose:
-            print(ddl_str)
-        qid = execute_athena_command(session_handle, ddl_str, database, output)
-        # if verbose:
-        #     print(qid)
+        ddl_str = "ALTER TABLE {0}.{1} {2} PARTITION ({3})".format(p_database, p_table, p_operation, partition_str)
+        if p_operation == 'ADD':
+            location = " LOCATION 's3://{0}/{1}{2}'".format(p_bucket, p_prefix, new_folder)
+            ddl_str += location
+        yield ddl_str
 
 
 def parse_parameters(argv):
@@ -65,9 +77,10 @@ def parse_parameters(argv):
     region = DEFAULT_REGION
     output = DEFAULT_OUTPUT
     verbose = False
+    drop = DEFAULT_DROP_MODE
 
     try:
-        opts, args = getopt.getopt(argv, "hvt:p:r:o:", ["table=", "profile=", "region=", "output="])
+        opts, args = getopt.getopt(argv, "hvt:p:r:o:d:", ["table=", "profile=", "region=", "output=", "drop="])
     except getopt.GetoptError:
         print(USAGE_STR)
         sys.exit(1)
@@ -85,8 +98,10 @@ def parse_parameters(argv):
             region = arg
         elif opt in ("-o", "--output"):
             output = arg
+        elif opt in ("-d", "--drop"):
+            drop = arg.lower() if arg.lower() in ('yes', 'no') else DEFAULT_DROP_MODE
 
-    return table, profile, region, output, verbose
+    return table, profile, region, output, drop, verbose
 
 def get_bucket_directories(p_session, p_bucket, p_prefix) -> set():
     s3_client = p_session.resource(service_name='s3')
